37b1395c95695b8379ea7069f479c95f5a2c7148
Diego Fincatto <diego@fincatto.com>
2013-04-02 20:54:26 -0300

    Agora a instituicao financeira conhece o interpretador da linha digitavel da mesma, fazendo com que o codigo fique menos propenso a erros por esquecimento.

--- src/main/java/com/ewmix/interpretador/codigodebarras/InstituicaoFinanceira.java
+++ src/main/java/com/ewmix/interpretador/codigodebarras/InstituicaoFinanceira.java
@@ -1,17 +1,25 @@
 package com.ewmix.interpretador.codigodebarras;
 
+import com.ewmix.interpretador.codigodebarras.impl.InterpretadorHsbc;
+
 public enum InstituicaoFinanceira {
 
-    HSBC("399");
+    HSBC("399", InterpretadorHsbc.class);
 
     private final String codigo;
+    private final Class<? extends Interpretador> interpretador;
 
-    private InstituicaoFinanceira(final String codigo) {
+    private InstituicaoFinanceira(final String codigo, final Class<? extends Interpretador> interpretador) {
         this.codigo = codigo;
+        this.interpretador = interpretador;
     }
 
     public String getCodigo() {
         return this.codigo;
+    }
+
+    public Class<? extends Interpretador> getInterpretador() {
+        return this.interpretador;
     }
 
     public static InstituicaoFinanceira valueOfCodigo(String codigo) {
