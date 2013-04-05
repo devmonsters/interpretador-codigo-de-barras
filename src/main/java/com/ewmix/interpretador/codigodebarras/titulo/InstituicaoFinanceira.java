d76337a4ba30fb1a2c17e89747bbfc8e1b04004a
Diego Fincatto <diego@fincatto.com>
2013-04-05 08:41:25 -0300

    Criado interpretador de boletos para o bradesco.

--- src/main/java/com/ewmix/interpretador/codigodebarras/titulo/InstituicaoFinanceira.java
+++ src/main/java/com/ewmix/interpretador/codigodebarras/titulo/InstituicaoFinanceira.java
@@ -1,29 +1,31 @@
 package com.ewmix.interpretador.codigodebarras.titulo;
 
+import com.ewmix.interpretador.codigodebarras.titulo.impl.InterpretadorTituloBradesco;
 import com.ewmix.interpretador.codigodebarras.titulo.impl.InterpretadorTituloGenerico;
 import com.ewmix.interpretador.codigodebarras.titulo.impl.InterpretadorTituloHsbc;
 
 public enum InstituicaoFinanceira {
-
+    
     GENERICO("000", InterpretadorTituloGenerico.class),
+    BRADESCO("237", InterpretadorTituloBradesco.class),
     HSBC("399", InterpretadorTituloHsbc.class);
-
+    
     private final String codigo;
     private final Class<? extends AbstractInterpretadorTitulo> interpretador;
-
+    
     private InstituicaoFinanceira(final String codigo, final Class<? extends AbstractInterpretadorTitulo> interpretador) {
         this.codigo = codigo;
         this.interpretador = interpretador;
     }
-
+    
     public String getCodigo() {
         return this.codigo;
     }
-
+    
     public Class<? extends AbstractInterpretadorTitulo> getInterpretador() {
         return this.interpretador;
     }
-
+    
     public static InstituicaoFinanceira valueOfCodigo(final String codigo) {
         if (!codigo.matches("[0-9]{3}")) {
             throw new IllegalArgumentException("O c\u00f3digo da institui\u00e7\u00e3o financeira deve ser 3 d\u00edgitos num\u00e9ricos.");
